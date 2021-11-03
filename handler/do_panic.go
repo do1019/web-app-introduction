fc716e230c3aa8e9461ba3bfac132a87017456d2
dogata <dogata@student.42tokyo.jp>
2021-11-04 06:16:45 +0900

    test

--- handler/do_panic.go
+++ handler/do_panic.go
@@ -13,7 +13,7 @@ func NewDoPanicHandler() *DoPanicHandler {
 }
 
 func (d *DoPanicHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//middleware.device_os_analysis(r)
+	middleware.device_os_analysis(r)
 	//fmt.Println("loop")
 	panic("do-panic!")
 }
