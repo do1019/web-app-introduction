6cb37d8075ab2cb7edbc2996e975af469d54b40b
dogata <dogata@student.42tokyo.jp>
2021-11-16 04:54:01 +0900

    step1 done

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/do1019/web-app-introduction
 
 go 1.17
-
-require github.com/mileusna/useragent v1.0.2
--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -3,23 +3,16 @@ package middleware
 import (
 	"log"
 	"net/http"
-	//"fmt"
 )
 
 func Recovery(next http.Handler) http.Handler {
-	//fmt.Println(1)
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		// TODO: ここに実装をする
-		// 無名関数, クロージャ
-		//fmt.Println(3)
 		defer func() {
 			if err := recover(); err != nil {
 				log.Println(err);
 			}
 		} ()
 		next.ServeHTTP(w, r)
-		//fmt.Println(ここまで来ない)
 	}
-	//fmt.Println(2)
 	return http.HandlerFunc(fn)
 }
