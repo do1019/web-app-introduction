80868cea616d0210a951bc7390638ab787f8a317
dogata <dogata@student.42tokyo.jp>
2021-11-16 13:12:50 +0900

    test

--- handler/middleware/basic_auth.go
+++ handler/middleware/basic_auth.go
@@ -12,10 +12,14 @@ type AuthInfo struct {
 }
 
 func ObtainIdAndPassFromEnviron() *AuthInfo {
-	return &AuthInfo{
-		os.Getenv("BASIC_AUTH_USER_ID"),
-		os.Getenv("BASIC_AUTH_PASSWORD"),
-	}
+	ai := &AuthInfo{}
+	ai.UserID = os.Getenv("BASIC_AUTH_USER_ID")
+	ai.Password = os.Getenv("BASIC_AUTH_PASSWORD")
+	return ai
+	// return &AuthInfo{
+	// 	os.Getenv("BASIC_AUTH_USER_ID"),
+	// 	os.Getenv("BASIC_AUTH_PASSWORD"),
+	// }
 }
 
 func (a *AuthInfo) AccessRestriction(next http.Handler) http.Handler {
@@ -25,7 +29,7 @@ func (a *AuthInfo) AccessRestriction(next http.Handler) http.Handler {
 			subtle.ConstantTimeCompare([]byte(userID), []byte(a.UserID)) != 1 ||
 			subtle.ConstantTimeCompare([]byte(pass), []byte(a.Password)) != 1 {
 			// Code for testing login in a browser.
-			// w.Header().Add("WWW-Authenticate", `Basic realm="my private area"`)
+			w.Header().Add("WWW-Authenticate", `Basic realm="my private area"`)
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
